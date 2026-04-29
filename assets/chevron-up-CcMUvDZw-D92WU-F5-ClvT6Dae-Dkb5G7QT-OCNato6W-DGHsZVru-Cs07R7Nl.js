e7105b08be5cccb246697b1728defd9b2abab5a0
wangxue <as5551238@126.com>
2026-04-29 08:49:47 +0800

    deploy: fix Admin crash + restore proper build pipeline

--- assets/chevron-up-CcMUvDZw-D92WU-F5-ClvT6Dae-Dkb5G7QT-OCNato6W-DGHsZVru-Cs07R7Nl.js
+++
@@ -1,6 +0,0 @@
-import{$ as e}from"./index-JAAgd3gI.js";/**
- * @license lucide-react v1.8.0 - ISC
- *
- * This source code is licensed under the ISC license.
- * See the LICENSE file in the root directory of this source tree.
- */const o=[["path",{d:"m18 15-6-6-6 6",key:"153udz"}]],r=e("chevron-up",o);export{r as e};
